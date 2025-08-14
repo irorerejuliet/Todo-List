56eca62106c943e6cdf5d10763171181dcd862e7
irorere juliet <irorerejuliet@gmail.com>
2025-08-14 23:35:55 +0100

    finish look

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ function displayTodo(todoValue){
     let todowrapper = document.createElement("div");
     todowrapper.classList.add("todo-wrapper");
 
-    todowrapper.innerHTML -
-      `<div class="todo bg-white flex items-center justify-between mx-20 text-2xl py-2 px-2  rounded">
+    todowrapper.innerHTML =
+      `<div class="todo bg-white flex items-center justify-between mx-20 text-2xl py-2 px-2 my-2 rounded">
         <p>${inputTodo.value}</p>
         <div class="actions">
           <input type="checkbox" class="mr-4">
-          <button class="bg-red-700 px-2 py-y text-white rounded ">Delect</button>
+          <button class="delete-btn bg-red-700 px-2 py-y text-white rounded ">Delete</button>
         </div>
       </div>`;
+
+      let deleteBtn = todowrapper.querySelector(".delete-btn")
+      deleteBtn.addEventListener("click", function delTodo() {
+        todowrapper.remove();
+
+      })
 
       todoContainer.appendChild(todowrapper);
 }
